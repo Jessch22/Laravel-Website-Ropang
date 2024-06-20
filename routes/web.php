ca556babccefb9d0a038cb13a62de279439692fd
Jessch22 <jessica.535220187@stu.untar.ac.id>
2024-06-21 00:44:55 +0700

    edit controller and fix bug

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\homeController;
 use App\Http\Controllers\AdminMenuController;
 
+//HOME
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+//BOOK A TABLE AND CONTACT
+Route::post('/book-table', [HomeController::class, 'bookTable'])->name('bookTable');
+Route::post('/contact', [HomeController::class, 'storeContact'])->name('contact.store');
 
 // MENU
 Route::get('/admin/menus', [AdminMenuController::class, 'index'])->name('admin.menus.index');
 Route::get('/admin/menus/create', [AdminMenuController::class, 'create'])->name('admin.menus.create');
 Route::post('/admin/menus', [AdminMenuController::class, 'store'])->name('admin.menus.store');
+
 Route::get('/admin/menus/{menu}/edit', [AdminMenuController::class, 'edit'])->name('admin.menus.edit');
 Route::put('/admin/menus/{menu}', [AdminMenuController::class, 'update'])->name('admin.menus.update');
 Route::delete('/admin/menus/{menu}', [AdminMenuController::class, 'destroy'])->name('admin.menus.destroy');
