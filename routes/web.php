09d3ce65f38e3163c063d2c1ac7a4aab437a61dd
Jessch22 <jessica.535220187@stu.untar.ac.id>
2024-06-27 23:17:57 +0700

    Add dropdown

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,17 @@ Route::post('/contacts', [IndexController::class, 'storeContact'])->name('storeC
 //ADMIN
 Route::get('/admin', [AdminController::class, 'index'])->name('screens.admindashboard');
 Route::post('/updateUserRole', [AdminController::class, 'updateRole'])->name('updateUserRole');
+Route::post('/updateReserveStatus', [AdminController::class, 'updateReserveStatus'])->name('updateReserveStatus');
+Route::post('/updateContactStatus', [AdminController::class, 'updateContactStatus'])->name('updateContactStatus');
+
 
 Route::delete('/admin/menus/{id}', [AdminController::class, 'destroy'])->name('admin.menus.destroy');
 Route::delete('admin/users/{id}', [AdminController::class, 'destroyUser'])->name('admin.users.destroy');
+Route::delete('admin/reservation/{id}', [AdminController::class, 'destroyReservation'])->name('admin.reservation.destroy');
+Route::delete('admin/contact/{id}', [AdminController::class, 'destroyContact'])->name('admin.contact.destroy');
 
 Route::get('/admin/menus/{id}/edit', [AdminController::class, 'edit'])->name('admin.menus.edit');
 Route::put('/admin/menus/{id}', [AdminController::class, 'update'])->name('admin.menus.update');
-
-Route::get('/admin/users/{id}/edit', [AdminController::class, 'editUser'])->name('admin.users.edit');
-Route::put('/admin/users/{id}', [AdminController::class, 'updateUser'])->name('admin.users.update');
-Route::delete('/admin/users/{id}', [AdminController::class, 'destroyUser'])->name('admin.users.destroy');
 
 //FOOTER - OTHER PAGE
 Route::get('/FAQ', function () {
