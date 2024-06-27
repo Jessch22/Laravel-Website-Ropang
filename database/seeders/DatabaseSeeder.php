72e81475305e1fb57732ffdc4171ef9f5fa0ef75
Jessch22 <jessica.535220187@stu.untar.ac.id>
2024-06-28 03:31:54 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,7 +9,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            // ContactSeeder::class,
+            ContactSeeder::class,
+            ReservationSeeder::class,
+            UserSeeder::class,
+            MenuSeeder::class
         ]);
     }
 }
