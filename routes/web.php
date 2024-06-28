0aa007894a60623f78c49d3beb968aecd5aaee4b
Jessch22 <jessica.535220187@stu.untar.ac.id>
2024-06-28 09:22:46 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\indexController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\AdminMenuController;
