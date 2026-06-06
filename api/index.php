2244217a68d9321fd1d2b072278d3cc407b684be
Jessch22 <jessicaho590@gmail.com>
2026-06-06 16:06:27 +0700

    Fix bugs, configure Vercel, and update README

--- api/index.php
+++ api/index.php
@@ -1,2 +1,6 @@
 <?php
+// Mencegah PHP 8.5 menampilkan pesan peringatan (Deprecated) ke layar website
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
+ini_set('display_errors', '0');
+
 require __DIR__ . '/../public/index.php';
